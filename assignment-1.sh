95f19980e47883a8e3242596b906e862771c2e80
Halil Şenaydın <70847361+halilsenaydin@users.noreply.github.com>
2023-12-14 00:11:40 +0300

    Add files via upload

---
+++ assignment-1.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo -e "Merhaba, sn. yürütücü. Bu ödev, kabuk programlamadaki değişkenleri anlamayı ve uygulamaya dökmeyi amaçlamaktadır. \n";
+
+echo -n "Lütfen isminizi girin: ";
+read name;
+
+echo "Merhaba $name !  Benim ismim de Halil, tanıştığıma çok memnun oldum.";
